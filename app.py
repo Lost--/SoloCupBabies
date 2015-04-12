a588ace6b3b5f04335355f20ed0572569fed05c6
lost-- <belmontypython.cyler@gmail.com>
2015-04-12 09:04:44 -0400

    Added flask thing

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from werkzeug import secure_filename
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        else:
+            file_type_error = 'File type is not supported. Please use JPG or JPEG'
+
+    return render_template('upload.html', file_type_error = file_type_error)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/help')
+def get_help():
+	return 'There is no God'
+
+if __name__ == '__main__':
+    app.run(debug=True)
